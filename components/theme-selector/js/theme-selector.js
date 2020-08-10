2220560303566a28b543f4c18c52f69cc7404ca5
JoshuaHolloway <jhollow6@asu.edu>
2020-08-10 10:34:48 -0500

    hover 3D effect in project.html

--- components/theme-selector/js/theme-selector.js
+++ components/theme-selector/js/theme-selector.js
@@ -20,8 +20,23 @@ const set_css_var_val = (css_var_name, new_val) => {
 
 const theme_buttons = Array.from(document.querySelectorAll('.Color-Options-Container > button'));
 console.log(theme_buttons);
+theme_buttons.forEach(item => {
+    item.addEventListener('click', event => {
+        if (theme_color.id === 'Theme-Blue') {
+            const new_css_var_val = get_css_var_val('--background-color-blue');
+            set_css_var_val('--background-color-primary', new_css_var_val);
 
-// if (theme_color.id === 'Theme-Blue') {
-//     const new_css_var_val = get_css_var_val('--background-color-blue');
-//     set_css_var_val('--background-color-primary', new_css_var_val);
-// }
+            // Set active class
+            item.classList.toggle('Active-Theme');
+
+            // TODO: Toggle this off when you select another element!
+            // TODO: Toggle this off when you select another element!
+            // TODO: Toggle this off when you select another element!
+            // TODO: Toggle this off when you select another element!
+            // TODO: Toggle this off when you select another element!
+            
+        } else if (theme_color.id === 'Theme-Yellow') {
+    });
+});
+
+
